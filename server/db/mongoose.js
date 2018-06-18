d9e80040ea755fb93a60f7902ebf0748799f7380
Jestone Aris Canuel <jestonecanuel@gmail.com>
2018-06-19 01:38:26 +0800

    Update port db connections

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -1,7 +1,15 @@
 var mongoose = require('mongoose');
+const port = process.env.PORT || 3000;
 
 mongoose.Promise = global.Promise;
-mongoose.connect('mongodb://node-todo:node-todo12345@ds159400.mlab.com:59400/mongo-data' || 'mongodb://localhost:27017/TodoApp');
+
+if(port !== 3000) {
+  var db = 'mongodb://node-todo:node-todo12345@ds159400.mlab.com:59400/mongo-data';
+} else {
+  var db = 'mongodb://localhost:27017/TodoApp';
+}
+
+mongoose.connect(db);
 
 module.exports = {
   mongoose
