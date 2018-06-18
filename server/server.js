ce2030bbfe12dd1ade0c7141521e5da7c289f836
Jestone Aris Canuel <jestonecanuel@gmail.com>
2018-06-19 00:40:38 +0800

    Add test cases for GET todo/:id

--- server/server.js
+++ server/server.js
@@ -51,8 +51,9 @@ app.get('/todos/:id', (req, res) => {
    }
 
    Todo.findById(id).then((todos) => {
-     if(!id){
-       return res.status(400).send();
+
+     if(!todos){
+       return res.status(404).send();
      }
 
      res.send({todos});
