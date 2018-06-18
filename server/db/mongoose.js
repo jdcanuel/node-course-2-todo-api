a6719714dada9ed0d4f8299d0196f0cae79f1842
Jestone Aris Canuel <jestonecanuel@gmail.com>
2018-06-19 01:09:39 +0800

    Setup DB in mongolab

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -1,7 +1,7 @@
 var mongoose = require('mongoose');
 
 mongoose.Promise = global.Promise;
-mongoose.connect('mongodb://jdcanuel:JestoneCanuel020688@ds159400.mlab.com:59400/mongo-data' || 'mongodb://localhost:27017/TodoApp');
+mongoose.connect('mongodb://node-todo:node-todo12345@ds159400.mlab.com:59400/mongo-data' || 'mongodb://localhost:27017/TodoApp');
 
 module.exports = {
   mongoose
