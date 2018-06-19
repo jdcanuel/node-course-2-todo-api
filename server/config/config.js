8f7ce2ae8945d87a4a4e97bca3fdb5a0f99e6258
Jestone Aris Canuel <jestonecanuel@gmail.com>
2018-06-20 01:00:30 +0800

    update mlab

--- server/config/config.js
+++ server/config/config.js
@@ -1,8 +1,11 @@
 var env = process.env.NODE_ENV || 'development';
+
 if(env === 'development') {
   process.env.PORT = 3000;
   process.env.MONGODB_URI = 'mongodb://localhost:27017/TodoApp';
 } else if(env === 'test') {
   process.env.PORT = 3000;
   process.env.MONGODB_URI = 'mongodb://localhost:27017/TodoAppTest';
+} else if(env === 'production') {
+  process.env.MONGODB_URI = 'mongodb://node-todo:node-todo12345@ds159400.mlab.com:59400/mongo-data';
 }
--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -3,13 +3,13 @@ const port = process.env.PORT || 3000;
 
 mongoose.Promise = global.Promise;
 
-if(port !== 3000) {
-  var db = 'mongodb://node-todo:node-todo12345@ds159400.mlab.com:59400/mongo-data';
-} else if (port === 3000){
-  var db = process.env.MONGODB_URI;
-}
+// if(port !== 3000) {
+//   var db = 'mongodb://node-todo:node-todo12345@ds159400.mlab.com:59400/mongo-data';
+// } else if (port === 3000){
+//   var db = process.env.MONGODB_URI;
+// }
 
-mongoose.connect(db);
+mongoose.connect(process.env.MONGODB_URI);
 
 module.exports = {
   mongoose
